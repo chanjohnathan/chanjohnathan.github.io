8a7048bccc83d5e5e5b6ef0c3155feda4ad64ca8
John Chan <johnathanc@gmail.com>
2015-02-26 05:38:37 -0500

    remove code

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-// $(".text-swap")
-
-
-// $('.text-swap').each(function(i, obj) {
-//     var $self = $(this);
-// 	var detectEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-
-//     if($self.css("display") == "none") {
-//       $self.addClass("animated fadeIn").css("display", "inline").one(detectEnd, function(){
-//       	$self.removeClass("animated fadeIn");
-//       	$self.addClass("animated fadeOut").one(detectEnd, function() {
-//       		$self.removeClass("animated fadeOut").css("display", "none");
-//       	});
-//       });
-//     }
-// });
-
-
-// $(document).ready(function (){
-// 	var animateText = "animated fadeIn";
-
-// 	$(".text-swap").each(function(index) {
-
-// 		$(this).addClass(animateText).css("visibility", "visble");
-// 	});
-// });
-
-
-//Loop through each .text-swap
-
-//Add the animation and the visiblity
-
-//Detect when it ends, remove the animation class and the visiblity
-// $('#yourElement').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
-
-
 (function loop() {
   $('.text-swap').each(function() {
     var $self = $(this);
@@ -43,35 +7,3 @@
     });
   }).parent().promise().done(loop);
 }());
-
-
-// (function loop() {
-//   $('.text-swap').each(function() {
-//     var $self = $(this);
-//     var animateTextIn = "animated fadeIn";
-//     var animateTextOut = "animated fadeOut";
-//     var detectEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-//     $self.parent().queue(function (n) {
-//       $self.show().addClass(animateTextIn).one(detectEnd, setTimeout(function() {
-//    		$self.removeClass(animateTextIn).addClass(animateTextOut)
-// }, 6200)).fadeOut(8000, n)
-//     });
-//   }).parent().promise().done(loop);
-// }());
-
-
-
-// (function loop() {
-//   $('.text-swap').each(function() {
-//     var $self = $(this);
-//     var detectEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-//     $self.parent().queue(function (n) {
-//       $self.addClass("animated shake").css("display", "inline").one(detectEnd, function(){
-//       	$self.removeClass("animated shake");
-//       	$self.addClass("animated fadeOut").one(detectEnd, function() {
-//       		$self.removeClass("animated fadeOut").css("display", "none");
-//       	});
-//       });
-//     });
-//   }).parent().promise().done(loop);
-// }());
