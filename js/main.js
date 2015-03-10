122d345247144e0cab348c28c2870d6b4342bd9e
John Chan <johnathanc@gmail.com>
2015-03-10 19:29:07 -0400

    fix popover

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-var detectEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-var animateMouse = "animated pulse";
-var animateClick = "animated swing";
+var detectEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+var animateMouse = 'animated pulse';
+var animateClick = 'animated swing';
 
 $(document).ready(function(){
-
+	// ********************************
 	// Social icon animations
-	$(".social-media").on("mouseover", function (){
+	// ********************************
+	$('.social-media').on('mouseover', function (){
 		$(this).addClass(animateMouse);
 	});
-	$(".social-media").on("mouseout", function (){
+	$('.social-media').on('mouseout', function (){
 		$(this).removeClass(animateMouse);
 	});
-	$(".social-media").click(function (){
+	// Removes the animation class when it's done animating
+	$('.social-media').click(function (){
 		$(this).addClass(animateClick).one(detectEnd, function(){
 			$(this).removeClass(animateClick);
 		});
 	});
 
-
+	// ********************************
 	// Popover
-	$("#more-hci").popover("hide");
+	// ********************************
+	// Init popover
+	$('#more-hci').popover({
+		viewport: '.container'
+	});
+	// Hide on click outside
 	$('body').on('click', function (e) {
 	    $('[data-toggle="popover"]').each(function () {
 	        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
@@ -27,6 +34,19 @@ $(document).ready(function(){
 	        }
 	    });
 	});
+	// // Change popover placement depending on screen size
+	// var $window = $(window);
+	// function checkWidth() {
+	// 	var windowsize = $window.width();
+	// 	if (windowsize < 768) {
+	// 		$('#more-hci').attr('data-placement','bottom');
+	// 	}
+	// 	else {
+	// 		$('#more-hci').attr('data-placement','right');
+	// 	}
+	// }
+	// checkWidth();
+	// $window.resize(checkWidth);
 });
 
 // Text swap
